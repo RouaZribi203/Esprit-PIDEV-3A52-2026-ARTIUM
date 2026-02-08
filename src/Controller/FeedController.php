34f3ea1a0047192dfbd82cacd9db94fc0fc3d741
RouaZribi203 <roua.zribi@esprit.tn>
2026-02-08 21:20:50 +0100

    second commit

--- src/Controller/FeedController.php
+++ src/Controller/FeedController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class HomeController extends AbstractController
+final class FeedController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/feed', name: 'app_feed')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('Front Office/feed/feed.html.twig', [
+            'controller_name' => 'FeedController',
         ]);
     }
 }

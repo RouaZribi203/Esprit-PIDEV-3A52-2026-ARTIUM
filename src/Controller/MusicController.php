cdca9ff2b3d4c5e87d28770202dec243b12ef3f2
salim <salim.benbrahim@esprit.tn>
2026-02-11 20:12:53 +0100

    crud tri recherche  fonctionnel front+back
    mezel ken controle de saisie, rabi ysahel

--- src/Controller/MusicController.php
+++ src/Controller/MusicController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Repository\MusiqueRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class MusicController extends AbstractController
 {
     #[Route('/musique', name: 'musiques')]
-    public function index(): Response
+    public function index(Request $request, MusiqueRepository $musiqueRepository): Response
     {
+        // Get search and sort parameters
+        $searchTerm = $request->query->get('search');
+        $sortBy = $request->query->get('sort', 'date');
+        $sortOrder = $request->query->get('order', 'DESC');
+        
+        // Fetch music with search/sort
+        $musiques = $musiqueRepository->searchAndFilter($searchTerm, $sortBy, $sortOrder);
+        
         return $this->render('music/music.html.twig', [
-            'controller_name' => 'MusicController',
+            'musiques' => $musiques,
+            'searchTerm' => $searchTerm,
+            'sortBy' => $sortBy,
+            'sortOrder' => $sortOrder,
         ]);
     }
 }

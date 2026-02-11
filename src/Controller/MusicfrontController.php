c792bb4b328c3886880575eaf974f59d514e2d0c
salim <salim.benbrahim@esprit.tn>
2026-02-12 00:39:10 +0100

    musique (tout fonctionne)
    affichage sur user-musiques (user normal)

--- src/Controller/MusicfrontController.php
+++ src/Controller/MusicfrontController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\MusiqueRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,14 @@ use Symfony\Component\Routing\Attribute\Route;
 final class MusicfrontController extends AbstractController
 {
     #[Route('/user-musiques', name: 'app_musicfront')]
-    public function index(): Response
+    public function index(MusiqueRepository $musiqueRepository): Response
     {
+        // Fetch all available songs
+        $musiques = $musiqueRepository->findAll();
+        
         return $this->render('Front Office/music/musicfront.html.twig', [
             'controller_name' => 'MusicfrontController',
+            'musiques' => $musiques,
         ]);
     }
 }

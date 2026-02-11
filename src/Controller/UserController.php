29954164a4f9f9d1a193d7d1df9b38bba3dbbcc2
RouaZribi203 <roua.zribi@esprit.tn>
2026-02-11 20:37:07 +0100

    commit1

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,28 +2,130 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use App\Enum\Role;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
+
+#[Route('/user')]
 final class UserController extends AbstractController
 {
-    #[Route('/amateurs', name: 'amateurs')]
-    public function index(): Response
+    #[Route('/', name: 'app_user_index', methods: ['GET'])]
+    public function index(UserRepository $userRepository): Response
     {
-        return $this->render('user/user.html.twig', [
-            'controller_name' => 'UserController',
-            'type' => 'mateur',
-        ]);
-    }
-    #[Route('/artistes', name: 'artistes')]
-    public function indexx(): Response
-    {
-        return $this->render('user/user.html.twig', [
-            'controller_name' => 'UserController',
-            'type' => 'rtiste',
+        $users = $userRepository->findAll();
+
+        return $this->render('user/index.html.twig', [
+            'users' => $users,
+            'page_title' => 'Tous les utilisateurs',
         ]);
     }
 
-    
+    #[Route('/artistes', name: 'app_user_artistes', methods: ['GET'])]
+    public function artistes(UserRepository $userRepository): Response
+    {
+        $users = $userRepository->findBy(['role' => Role::ARTISTE]);
+
+        return $this->render('user/index.html.twig', [
+            'users' => $users,
+            'page_title' => 'Liste des Artistes',
+            'current_filter' => 'ARTISTE'
+        ]);
+    }
+
+    #[Route('/amateurs', name: 'app_user_amateurs', methods: ['GET'])]
+    public function amateurs(UserRepository $userRepository): Response
+    {
+        $users = $userRepository->findBy(['role' => Role::AMATEUR]);
+
+        return $this->render('user/index.html.twig', [
+            'users' => $users,
+            'page_title' => 'Liste des Amateurs',
+            'current_filter' => 'AMATEUR'
+        ]);
+    }
+
+    #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user, ['is_edit' => false]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // Hash the password
+            $plainPassword = $form->get('mdp')->getData();
+            if ($plainPassword) {
+                $hashedPassword = $passwordHasher->hashPassword($user, $plainPassword);
+                $user->setMdp($hashedPassword);
+            }
+
+            // Set registration date
+            $user->setDateInscription(new \DateTime());
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Utilisateur créé avec succès !');
+
+            return $this->redirectToRoute('app_user_index');
+        }
+
+        return $this->render('user/new.html.twig', [
+            'form' => $form,
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_user_show', methods: ['GET'], requirements: ['id' => '\d+'])]
+    public function show(User $user): Response
+    {
+        return $this->render('user/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(UserType::class, $user, ['is_edit' => true]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Utilisateur modifié avec succès !');
+
+            return $this->redirectToRoute('app_user_index');
+        }
+
+        return $this->render('user/edit.html.twig', [
+            'form' => $form,
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/{id}/delete', name: 'app_user_delete', methods: ['POST'])]
+    public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        // Suppression directe sans vérification CSRF pour tester
+        try {
+            $entityManager->remove($user);
+            $entityManager->flush();
+            $this->addFlash('success', 'Utilisateur supprimé avec succès !');
+        } catch (\Exception $e) {
+            $this->addFlash('error', 'Erreur lors de la suppression: ' . $e->getMessage());
+        }
+
+        return $this->redirectToRoute('app_user_index');
+    }
 }

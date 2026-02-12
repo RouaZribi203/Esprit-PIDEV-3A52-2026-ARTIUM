a716d1c61421e466bb72e45eeae23ce771061cd6
khadija404 <150249323+khadija404@users.noreply.github.com>
2026-02-12 21:32:03 +0100

    filtres

--- src/Controller/FeedController.php
+++ src/Controller/FeedController.php
@@ -23,8 +23,19 @@ final class FeedController extends AbstractController
     #[Route('/feed', name: 'app_feed')]
     public function index(OeuvreRepository $oeuvreRepository, CollectionsRepository $collectionsRepository): Response
     {
-         $oeuvres = $oeuvreRepository->findAll();
-        
+        $oeuvres = $oeuvreRepository->findAll();
+        $peintures = $oeuvreRepository->findBy([
+            'type' => TypeOeuvre::PEINTURE
+       ]);
+
+        $sculptures = $oeuvreRepository->findBy([
+           'type' => TypeOeuvre::SCULPTURE
+       ]);
+
+        $photos = $oeuvreRepository->findBy([
+           'type' => TypeOeuvre::PHOTOGRAPHIE
+        ]);
+        $all = array_merge($peintures, $sculptures, $photos);
         // Process oeuvre images
         $processedOeuvres = [];
         foreach ($oeuvres as $oeuvre) {
@@ -55,12 +66,138 @@ final class FeedController extends AbstractController
 
         return $this->render('Front Office/feed/feed.html.twig', [
             'controller_name' => 'FeedController',
-            'oeuvres' => $oeuvres,
+            'oeuvres' => $all,
             'typeOeuvres' => TypeOeuvre::cases(),
             'collections' => $collectionsRepository->findAll(),
             'processedOeuvres' => $processedOeuvres,
         ]);
     }
+    #[Route('/feed_peintures', name: 'app_feed_peintures')]
+    public function indexPeintures(OeuvreRepository $oeuvreRepository, CollectionsRepository $collectionsRepository): Response
+    {
+        $peintures = $oeuvreRepository->findBy([
+            'type' => TypeOeuvre::PEINTURE
+       ]);
+        $processedOeuvres = [];
+        foreach ($peintures as $oeuvre) {
+            $image = $oeuvre->getImage();
+            
+            if ($image) {
+                // Handle resource streams
+                if (is_resource($image)) {
+                    rewind($image);
+                    $imageData = stream_get_contents($image);
+                } else {
+                    $imageData = $image;
+                }
+                
+                // Only process if we have actual image data
+                if ($imageData && strlen($imageData) > 0) {
+                    $imageBase64 = base64_encode($imageData);
+                    $finfo = new \finfo(FILEINFO_MIME_TYPE);
+                    $mimeType = $finfo->buffer($imageData);
+                    
+                    $processedOeuvres[$oeuvre->getId()] = [
+                        'imageBase64' => $imageBase64,
+                        'mimeType' => $mimeType ?: 'image/jpeg',
+                    ];
+                }
+            }
+        }
+
+        return $this->render('Front Office/feed/feed.html.twig', [
+            'controller_name' => 'FeedController',
+            'oeuvres' => $peintures,
+            'typeOeuvres' => TypeOeuvre::cases(),
+            'collections' => $collectionsRepository->findAll(),
+            'processedOeuvres' => $processedOeuvres,
+        ]);
+    }
+
+    #[Route('/feed_sculptures', name: 'app_feed_sculptures')]
+    public function indexSculptures(OeuvreRepository $oeuvreRepository, CollectionsRepository $collectionsRepository): Response
+    {
+        $sculptures = $oeuvreRepository->findBy([
+            'type' => TypeOeuvre::SCULPTURE
+       ]);
+        $processedOeuvres = [];
+        foreach ($sculptures as $oeuvre) {
+            $image = $oeuvre->getImage();
+            
+            if ($image) {
+                // Handle resource streams
+                if (is_resource($image)) {
+                    rewind($image);
+                    $imageData = stream_get_contents($image);
+                } else {
+                    $imageData = $image;
+                }
+                
+                // Only process if we have actual image data
+                if ($imageData && strlen($imageData) > 0) {
+                    $imageBase64 = base64_encode($imageData);
+                    $finfo = new \finfo(FILEINFO_MIME_TYPE);
+                    $mimeType = $finfo->buffer($imageData);
+                    
+                    $processedOeuvres[$oeuvre->getId()] = [
+                        'imageBase64' => $imageBase64,
+                        'mimeType' => $mimeType ?: 'image/jpeg',
+                    ];
+                }
+            }
+        }
+
+        return $this->render('Front Office/feed/feed.html.twig', [
+            'controller_name' => 'FeedController',
+            'oeuvres' => $sculptures,
+            'typeOeuvres' => TypeOeuvre::cases(),
+            'collections' => $collectionsRepository->findAll(),
+            'processedOeuvres' => $processedOeuvres,
+        ]);
+    }
+    #[Route('/feed_photos', name: 'app_feed_photos')]
+    public function indexPhotos(OeuvreRepository $oeuvreRepository, CollectionsRepository $collectionsRepository): Response
+    {
+        $photos = $oeuvreRepository->findBy([
+            'type' => TypeOeuvre::PHOTOGRAPHIE
+       ]);
+        $processedOeuvres = [];
+        foreach ($photos as $oeuvre) {
+            $image = $oeuvre->getImage();
+            
+            if ($image) {
+                // Handle resource streams
+                if (is_resource($image)) {
+                    rewind($image);
+                    $imageData = stream_get_contents($image);
+                } else {
+                    $imageData = $image;
+                }
+                
+                // Only process if we have actual image data
+                if ($imageData && strlen($imageData) > 0) {
+                    $imageBase64 = base64_encode($imageData);
+                    $finfo = new \finfo(FILEINFO_MIME_TYPE);
+                    $mimeType = $finfo->buffer($imageData);
+                    
+                    $processedOeuvres[$oeuvre->getId()] = [
+                        'imageBase64' => $imageBase64,
+                        'mimeType' => $mimeType ?: 'image/jpeg',
+                    ];
+                }
+            }
+        }
+
+        return $this->render('Front Office/feed/feed.html.twig', [
+            'controller_name' => 'FeedController',
+            'oeuvres' => $photos,
+            'typeOeuvres' => TypeOeuvre::cases(),
+            'collections' => $collectionsRepository->findAll(),
+            'processedOeuvres' => $processedOeuvres,
+        ]);
+    }
+
+
 
     #[Route('/oeuvre/{id}/favorite', name: 'oeuvre_favorite')]
     public function favorite(Oeuvre $oeuvre, EntityManagerInterface $em, UserRepository $userRepository): Response
